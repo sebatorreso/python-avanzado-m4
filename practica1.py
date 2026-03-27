fefe2949577a5f820e1ee468bbab68f2a818c3ad
sebatorreso <storresopazo@gmail.com>
2026-03-26 21:22:49 -0300

    Se instancia la clase y se ejecuta

--- practica1.py
+++ practica1.py
@@ -9,6 +9,8 @@ class Persona:
     def saludar(self):
         print(f"Hola mi nombre es {self.nombre} y tengo {self.edad} años")
 
+    def cumplir_anios(self):
+        self.edad = self.edad + 1 
 
 
 
@@ -24,4 +26,8 @@ print(juanito.nombre, juanito.edad)
 print(carlos.nombre, carlos.edad)
 
 
+print(juanito.saludar())
+print(juanito.cumplir_anios())
+print(juanito.saludar())
+print(juanito.cumplir_anios())
 print(juanito.saludar())
