0c35342f5aafdbdbf3ad31156fe42cc207790678
sebatorreso <storresopazo@gmail.com>
2026-04-05 13:43:22 -0400

    POO

---
+++ clases/transacciones.py
@@ -0,0 +1,60 @@
+
+
+class Transaccion:
+
+    def __init__(self, monto, descripcion):
+        self.__monto = monto 
+        self.descripcion = descripcion 
+
+    def get_monto(self):
+        return self.__monto 
+    
+    def set_monto(self, monto):
+        if monto > 0:
+            self.__monto = monto
+        else:
+            print("Monto ingresao es 0 o menor")
+
+    def tipo(self):
+        return "Transacción"
+
+    def mostrar(self):
+        return print(f"{self.descripcion} - {self.get_monto()} - {self.tipo()}")
+
+
+
+class Ingreso(Transaccion):
+    # Polimorfismo de la clase Transaccion, del método tipo()
+    def tipo(self):
+        return "Ingreso"
+
+
+class Egreso(Transaccion):
+    
+    def tipo():
+        return "Egreso"
+
+
+
+class GestorFinanzas:
+    def __init__(self):
+        self.transacciones = []
+
+    def agregar(self):
+        pass
+
+    def calcular_balance(self):
+        pass
+
+    def guardar_en_archivo(self):
+        pass
+
+    def importar_del_archivo(self):
+        pass
+
+
+pago = Transaccion(100, "Pago de Servicio")
+pago.mostrar()
+
+
+
